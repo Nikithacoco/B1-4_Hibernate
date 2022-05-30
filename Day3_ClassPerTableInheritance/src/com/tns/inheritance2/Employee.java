c4372caa830e1861dd4eb05ad733cfff75e143ba
hp <hp@LAPTOP-44LCNLGG>
2022-05-30 22:07:20 +0530

    first commit

--- Day3_ClassPerTableInheritance/src/com/tns/inheritance2/Employee.java
+++ Day3_ClassPerTableInheritance/src/com/tns/inheritance2/Employee.java
@@ -1,11 +1,21 @@
 package com.tns.inheritance2;
 
 import java.io.Serializable;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 @Entity
-@Table(name="EMP_STORE1")
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="emp_type",discriminatorType=DiscriminatorType.STRING)
-@DiscriminatorValue("EMP")
+@Table(name="EMP_STORE2")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+
 public class Employee implements Serializable{
 	private static final long serialVersionUID=1L;
 	@Id
@@ -17,8 +27,7 @@ public class Employee implements Serializable{
 		return empID;
 	}
 	public void setEmpID(int empID) {
-		
-this.empID = empID;
+		this.empID = empID;
 	}
 	public String getName() {
 		return name;
--- Day3_ClassPerTableInheritance/src/com/tns/inheritance2/Manager.java
+++ Day3_ClassPerTableInheritance/src/com/tns/inheritance2/Manager.java
@@ -1,17 +1,36 @@
 package com.tns.inheritance2;
 
-import javax.persistence.DiscriminatorValue;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-@Entity
-@DiscriminatorValue("MGR")
-public class Manager extends Employee
-{
-	private static final long serialVersionUID=1L;
-	private String deptName;
-	public String getDeptName() {
-		return deptName;
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one Employee
+		Employee emp=new Employee();
+		emp.setName("Vinay");
+		emp.setSalary(25000);
+		em.persist(emp);
+		
+		//create one manager
+		Manager manager=new Manager();
+		manager.setName("Shiwani");
+		manager.setSalary(70000);
+		manager.setDeptName("Program Specialist");
+		em.persist(manager);
+		
+		em.getTransaction().commit();
+		System.out.println("added one emp and one manager");
+		em.close();
+		factory.close();
+		
+		
+
 	}
-	public void setDeptName(String deptName) {
-		this.deptName = deptName;
-	}
+
 }
